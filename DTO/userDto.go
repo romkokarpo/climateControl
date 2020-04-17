3079424769e4ce8309324aefef5e32221e19dbb6
Roman Karpinskyy <roman.karpinskyy.lv@gmail.com>
2020-04-18 02:55:55 +0300

    User registration implemented. User DTO added.

---
+++ DTO/userDto.go
@@ -0,0 +1,8 @@
+package DTO
+
+type UserDto struct {
+	FirstName string
+	LastName  string
+	Email     string
+	Password  string
+}
--- repositories/userRepository.go
+++ repositories/userRepository.go
@@ -2,9 +2,16 @@ package repositories
 
 import (
 	"climateControl/DAL"
+	"climateControl/DTO"
 	"climateControl/models"
+	"time"
+
+	"github.com/PeteProgrammer/go-automapper"
+	"golang.org/x/crypto/bcrypt"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type UserRepository struct {
@@ -31,4 +38,54 @@ func (repository *UserRepository) GetAllUsers() []*models.User {
 	}
 
 	return users
+}
+
+func (repository *UserRepository) GetUserById(id string) *models.User {
+	ctx := repository.dbContext
+	userId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		panic(err)
+	}
+
+	queryResult := ctx.Users.FindOne(*ctx.MongoContext, bson.M{"_id": userId})
+	user := models.User{}
+	queryResult.Decode(user)
+
+	return &user
+}
+
+func (repository *UserRepository) GetUserByEmail(email string) *models.User {
+	ctx := repository.dbContext
+	queryResult := ctx.Users.FindOne(*ctx.MongoContext, bson.M{"email": email})
+	user := models.User{}
+	queryResult.Decode(user)
+
+	return &user
+}
+
+func (repository *UserRepository) RegisterUser(model DTO.UserDto) *mongo.InsertOneResult {
+	var user models.User
+	automapper.MapLoose(model, user)
+
+	hash, err := hashPassword(model.Password)
+	if err != nil {
+		panic(err)
+	}
+
+	user.PasswordHash = hash
+	user.RegistrationDate = time.Now()
+	user.LastLoginDate = time.Now()
+
+	ctx := repository.dbContext
+	insertResult, err := repository.dbContext.Users.InsertOne(*ctx.MongoContext, user)
+	if err != nil {
+		panic(err)
+	}
+
+	return insertResult
+}
+
+func hashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
 }
